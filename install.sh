b547d97405b22df6a6f55eb6bd7aaf7cc2dfac28
alex1701c <alex1701c.dev@gmx.net>
2020-08-16 10:44:14 +0200

    Add install/uninstall scripts, add dependencies to readme, cleanup deb file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Exit immediately if something fails
+set -e
+
+mkdir build
+cd build
+cmake ../ \
+-DCMAKE_INSTALL_PREFIX=`kf5-config --prefix` \
+-DCMAKE_BUILD_TYPE=Release
+make -j$(nproc)
+sudo make 
+kquitapp5 krunner 2> /dev/null; kstart5 --windowclass krunner krunner > /dev/null 2>&1 &
+
